fd4fa7b3147172f7ab0183cf79930788b8cca30c
Azeez <isaac.douglas08@gmail.com>
2023-09-27 10:11:26 -0300

    finalizando tudo

--- playwright.config.ts
+++ playwright.config.ts
@@ -28,7 +28,9 @@ export default defineConfig({
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
-    headless: true
+    headless: true,
+    baseURL: 'http://localhost:8080',
+    screenshot: 'on'
   },
 
   /* Configure projects for major browsers */
@@ -38,25 +40,25 @@ export default defineConfig({
       use: { ...devices['Desktop Chrome'] },
     },
 
-    // {
-    //   name: 'firefox',
-    //   use: { ...devices['Desktop Firefox'] },
-    // },
+    {
+      name: 'firefox',
+      use: { ...devices['Desktop Firefox'] },
+    },
 
-    // {
-    //   name: 'webkit',
-    //   use: { ...devices['Desktop Safari'] },
-    // },
+    {
+      name: 'webkit',
+      use: { ...devices['Desktop Safari'] },
+    },
 
-    /* Test against mobile viewports. */
-    // {
-    //   name: 'Mobile Chrome',
-    //   use: { ...devices['Pixel 5'] },
-    // },
-    // {
-    //   name: 'Mobile Safari',
-    //   use: { ...devices['iPhone 12'] },
-    // },
+    // Test against mobile viewports. */
+    {
+      name: 'Mobile Chrome',
+      use: { ...devices['Pixel 5'] },
+    },
+    {
+      name: 'Mobile Safari',
+      use: { ...devices['iPhone 12'] },
+    },
 
     /* Test against branded browsers. */
     // {
--- tests/tasks.spec.ts
+++ tests/tasks.spec.ts
@@ -1,37 +1,81 @@
-import { test } from '@playwright/test'
+import { test, expect } from '@playwright/test'
+
 import { TaskModel } from './fixtures/task.model'
 import { deleteTaskByHelper, createTask } from './support/helpers'
-import { TaskPage } from './support/pages/tasks'
+import { TasksPage } from './support/pages/tasks'
 
-test('deve poder cadastrar uma nova tarefa', async function( { page, request } ) {
+import data from "./fixtures/tasks.json"
 
-    const task: TaskModel = {
-        name: 'Ler um livro de TypeScript',
-        is_done: false
-    }
+let tasksPage: TasksPage
 
-    await deleteTaskByHelper(request, task.name)
-    
-    const tasksPage : TaskPage = new TaskPage(page)
-
-    await tasksPage.go()
-    await tasksPage.create(task)
-    await tasksPage.shouldHaveText(task.name)
+test.beforeEach(({ page }) => {
+    tasksPage = new TasksPage(page)
 })
 
-test('Não deve permitir tarefa duplicada', async function( { page, request } ) {
+test.describe('cadastro', () => {
+    test('deve poder cadastrar uma nova tarefa', async function ({ request }) {
 
-    const task: TaskModel = {
-        name: 'Comprar Ketchup',
-        is_done: false
-    }
+        const task = data.success as TaskModel
 
-    await deleteTaskByHelper(request, task.name)
-    await createTask(request, task)
+        await deleteTaskByHelper(request, task.name)
 
-    const tasksPage : TaskPage = new TaskPage(page)
+        await tasksPage.go()
+        await tasksPage.create(task)
+        await tasksPage.shouldHaveText(task.name)
+    })
 
-    await tasksPage.go()
-    await tasksPage.create(task)
-    await tasksPage.alertHaveText('Task already exists!')
+    test('Não deve permitir tarefa duplicada', async function ({ request }) {
+
+        const task = data.duplicate as TaskModel
+
+        await deleteTaskByHelper(request, task.name)
+        await createTask(request, task)
+
+        await tasksPage.go()
+        await tasksPage.create(task)
+        await tasksPage.alertHaveText('Task already exists!')
+    })
+
+    test('Campo obrigatório', async function () {
+
+        const task = data.required as TaskModel
+
+        await tasksPage.go()
+        await tasksPage.create(task)
+
+        const validationMessage = await tasksPage.inputTaskName.evaluate(e => (e as HTMLInputElement).validationMessage)
+        expect(validationMessage).toEqual('This is a required field')
+    })
+})
+
+test.describe('atualiação', () => {
+    test('Deve concluir uma tarefa', async function ({ request }) {
+
+        const task = data.update as TaskModel
+
+        await deleteTaskByHelper(request, task.name)
+        await createTask(request, task)
+
+        await tasksPage.go()
+
+        await tasksPage.go()
+        await tasksPage.toggle(task.name)
+        await tasksPage.shouldBeDone(task.name)
+    })
+})
+
+test.describe('exclusão', () => {
+    test('Deve apagar uma tarefa', async function ({ request }) {
+
+        const task = data.delete as TaskModel
+
+        await deleteTaskByHelper(request, task.name)
+        await createTask(request, task)
+
+        await tasksPage.go()
+
+        await tasksPage.go()
+        await tasksPage.delete(task.name)
+        await tasksPage.shouldNotExist(task.name)
+    })
 })
